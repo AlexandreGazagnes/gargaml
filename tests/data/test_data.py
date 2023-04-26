734761eca829b0776137f4652a25192aeacd51b8
AlexandreGazagnes <a_syoez@yahoo.fr>
2023-04-26 10:05:49 +0200

    update tests

---
+++ tests/data/test_data.py
@@ -0,0 +1,21 @@
+import pytest
+
+from gargaml import Loads
+from gargaml.data import DataClass
+
+
+def test_init():
+
+
+    df = Loads.ames(X_y=False)
+
+    print(df.columns)
+
+    data = DataClass(df, y_name="target")
+
+    print(len(df))
+    print(data.X.shape)
+    print(data.X.train.shape)
+    print(data.train.X.shape)
+
+    print(data)
