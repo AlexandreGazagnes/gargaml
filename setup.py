a490b7a6cc23af29363f9fd6f318ba432f3307eb
AlexandreGazagnes <a_syoez@yahoo.fr>
2023-05-02 23:28:06 +0200

    update ops

--- setup.py
+++ setup.py
@@ -1,8 +1,26 @@
 from setuptools import setup, find_packages
 
-VERSION = "0.0.13"
+VERSION = "0.1.1"
 DESCRIPTION = "A personal ML lib"
 LONG_DESCRIPTION = "A personal ML lib"
+
+
+# def get_version(fname=os.path.join('gargaml', '__init__.py')):
+#     with open(fname) as f:
+#         for line in f:
+#             if line.startswith('__version__'):
+#                 return eval(line.split('=')[-1])
+
+
+# def get_long_description():
+#     descr = []
+#     for fname in ('README.md',):
+#         with open(fname) as f:
+#             descr.append(f.read())
+#     return '\n\n'.join(descr)
+
+
+# plz check https://github.com/PyCQA/pyflakes/blob/main/setup.py as a good fancy setup
 
 # Setting up
 setup(
@@ -62,7 +80,7 @@ setup(
         "mypy",
         "coverage",
     ],
-    keywords=["python", "machine learning"],
+    keywords=["python", "machine", "learning", "data",  "analysis", "EDA", "ML"],
     classifiers=[
         "Programming Language :: Python :: 3",
     ],
