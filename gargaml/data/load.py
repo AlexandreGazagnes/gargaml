65b0773e5d4ab16967324572289ec36c780438f4
AlexandreGazagnes <a_syoez@yahoo.fr>
2023-05-03 20:10:13 +0200

    update loaders

--- gargaml/data/load.py
+++ gargaml/data/load.py
@@ -7,69 +7,84 @@ import numpy as np
 import random
 
 
-def _load(data: str, X_y: bool, nan_rate: float):
-    # data
-    if data == "ames":
+from .utils import _preclean_seattle, _clean_columns, _add_nan, _do_sep_target
+
+
+def __sk_load(name: str, sep_target: bool = False, nan_rate: float = 0.05):
+    assert name in ["ames", "iris"]
+
+    # name / fectch
+    if name == "ames":
         data = fetch_california_housing()
     elif data == "iris":
         data = load_iris()
 
-    # X
-    X = data.data
-    y = data.target
-    df = pd.DataFrame(X, columns=data.feature_names)
-    df.columns = [i.lower() for i in df.columns]
+    # df
+    df = pd.DataFrame(data.data, columns=data.feature_names)
+    df["target"] = data.target
 
-    # nan_rate
+    # target name
+    if name == "iris":
+        replace_dict = {k: v for k, v in enumerate(data.target_names)}
+        df.target = df.target.astype(int).replace(replace_dict)
+
+    df.columns = map(_clean_columns, df.columns)
+
+    df = _add_nan(df, nan_rate=nan_rate)
+
+
+    return  _do_sep_target(df, sep_target, "target")
+
+
+def _ames(sep_target: bool = False, nan_rate: float = 0.0):
+    """ """
+
+    return __sk_load("ames", sep_target=sep_target, nan_rate=nan_rate)
+
+
+def _iris(sep_target: bool = False, nan_rate: float = 0.0):
+    """ """
+
+    return __sk_load("iris", sep_target=sep_target, nan_rate=nan_rate)
+
+
+def _seattle(
+    sep_target=False,
+    year: int = 2016,
+    preclean: bool = False,
+    nan_rate: float = 0.0,
+    *args,
+    **kwargs,
+):
+    """ """
+
+    # check
+    assert year in [2016, 2015]
     if nan_rate:
-        N = df.shape[0] * df.shape[1]
-        nan_numb = int(nan_rate * N)
+        raise AttributeError("not implemented : nan_rate in False, None, 0")
+    if year != 2016:
+        raise AttributeError("not implemented : year=2016 ")
 
-        for _ in range(nan_numb):
-            x, y = random.randint(0, df.shape[0] - 1), random.randint(
-                0, df.shape[1] - 1
-            )
-            df.iloc[x, y] = np.NaN
-
-    # y
-    df["target"] = y
-
-    # return X_y or df
-    if X_y:
-        return df.drop("target", axis=1), df["target"]
-
-    return df
-
-
-def _ames(X_y: bool = True, nan_rate: float = 0.0):
-    """ """
-
-    return _load("ames", X_y=X_y, nan_rate=nan_rate)
-
-
-def _iris(X_y: bool = True, nan_rate: float = 0.0):
-    """ """
-
-    return _load("iris", X_y=X_y, nan_rate=nan_rate)
-
-
-def _seattle(year="2016", *args, **kwargs):
-    """ """
-
-    assert year in ["2016", "2015"]
-
+    # dl
     _2016 = "https://gist.githubusercontent.com/AlexandreGazagnes/37b2f3c19da4c4dfc8e5e81cd883169f/raw/09cb1d4d4d3adf4f5838421afe1ca9c44a4dacc2/seattle_2016.csv"
+    df = pd.read_csv(_2016)
+    assert isinstance(df, pd.DataFrame)
 
-    if year == "2016":
-        df = pd.read_csv(_2016)
-        assert isinstance(df, pd.DataFrame)
-        return df
+    # clean
+    df.columns = map(_clean_columns, df.columns)
+    df.osebuildingid = df.osebuildingid.astype(str)
 
-    return NotImplementedError()
+    # preclean
+    if preclean:
+        df = _preclean_seattle(df)
+
+
+
+    return  _do_sep_target(df, sep_target, "siteenergyuse_kbtu")
 
 
 def _titanic(
-    X_y: bool = True,
+    sep_target: bool = False,
     nan_rate: float = 0.1,
 ):
     """ """
@@ -78,84 +93,134 @@ def _titanic(
 
     df = pd.read_csv(url)
 
-    if not X_y:
-        return df
+    df.columns = map(_clean_columns, df.columns)
 
-    return df.drop(columns="Survived"), df.Survived
+    return _do_sep_target(df, sep_target, "survived")
 
 
-def _hr(*args, **kwargs):
+
+
+
+def _hr(sep_target: bool = False, nan_rate: float = 0.1):
     """ """
 
     url = "https://gist.githubusercontent.com/AlexandreGazagnes/c52899a975ab6114f6ca83eeabd563b7/raw/d539a5028e53a2fa446c4dbdf142cc10066f8ec2/hr_datascience_train.csv"
 
-    return pd.read_csv(url)
+    df = pd.read_csv(url)
+    df.columns = map(_clean_columns, df.columns)
+
+    if not sep_target:
+        return df
+
+    # rename Salary as target
+    target = "salary"
+
+    return df.drop(columns=target), df[target]
 
 
-def _house(*args, **kwargs):
-    """ """
+def _house(sep_target: bool = False, nan_rate: float = 0.1):
+    """house price regression from kaggle"""
+
+    if nan_rate:
+        raise AttributeError("nan_rate invalid for this df, retry nan_rate=None")
 
     url = "https://gist.githubusercontent.com/AlexandreGazagnes/796384619817c9e93e60a288abc188ab/raw/989be9dfbb83e40ac3374bb2d629225be6fcaa1b/dataset_house_price_raw"
 
-    return pd.read_csv(url)
+    df = pd.read_csv(url)
+
+    df.columns = map(_clean_columns, df.columns)
+
+    return  _do_sep_target(df, sep_target, "saleprice")
 
 
-def _wine(X_y: bool = True, nan_rate: float = 0.1, *agrs, **kwargs):
+def _wine(
+    sep_target: bool = False,
+    nan_rate: float = 0.1,
+):
     """ """
+
+    if nan_rate:
+        raise AttributeError("nan_rate invalid for this df, retry nan_rate=None")
 
     url = "https://gist.githubusercontent.com/AlexandreGazagnes/e3e3a6ece82363fd1cafdd0f32563fce/raw/84c63cbee273574023b48bb750e91616c5253c74/wine-quality"
 
-    return pd.read_csv(url)
+    df = pd.read_csv(url)
+
+    df.columns = map(_clean_columns, df.columns)
+
+    return  _do_sep_target(df, sep_target, "quality")
 
 
-def _minst(X_y: bool = True, *args, **kwargs):
+
+
+
+def _food(
+    sep_target: bool = False,
+    y_type="score",
+    nan_rate=None,
+):
     """ """
 
-    return None
-
-
-def _food(*agrs, **kwargs):
-    """ """
+    # val
+    assert y_type in ("score", "grade")
+    if nan_rate:
+        raise AttributeError("nan_rate invalid for this df, retry nan_rate=None")
 
     logging.warning("Expected time : 30 sec to download")
 
     url = "https://gist.githubusercontent.com/AlexandreGazagnes/2477fc721596f91a9fb9da9c544949f8/raw/a27b65b141896cca35f5ab6d75b5fb3dd1b39683/food"
+    df = pd.read_csv(url)
 
-    return pd.read_csv(url)
+    df.columns = map(_clean_columns, df.columns)
+
+    if not sep_target:
+        return df
+
+    # target
+    target_cols = ["nutrition_grade_fr", "nutrition-score-fr_100g"]
+    target = "nutrition_grade_fr" if "grade" in y_type else "nutrition-score-fr_100g"
+
+    return df.drop(columns=target_cols), df[target]
 
 
-def _fashion(*agrs, **kwargs):
-    """ """
+# def _fashion():
+#     """ """
 
-    return None
+#     return None
+
+
+# def _mnist(sep_target: bool = True, *args, **kwargs):
+#     """ """
+
+#     return None
 
 
 class Load:
     """ """
 
     ames = _ames
-    boston = _ames
-    fashion = _fashion
+    # boston = _ames
+    # fashion = _fashion
     food = _food
     house = _house
     hr = _hr
     iris = _iris
-    minst = _minst
+    # mnist = _mnist
     seattle = _seattle
     titanic = _titanic
     wine = _wine
     list_all = sorted(
         [
-            "boston",
+            # "boston",
             "ames",
             "seattle",
             "hr",
             "titanic",
             "house",
-            "mnist",
+            # "mnist",
             "food",
             "wine",
             "iris",
-            "fashion",
+            # "fashion",
         ]
     )
---
+++ gargaml/data/utils.py
@@ -0,0 +1,97 @@
+import logging, random
+import pandas as pd
+
+import numpy as np
+
+def _do_sep_target(df, sep_target, target) : 
+    """ """
+
+    if not sep_target:
+        return df
+    return df.drop(columns=target), df[target]
+
+
+def _clean_columns(txt: str) -> str:
+    """_clean columns for a df"""
+
+    txt = txt.lower().strip()
+    replace = [
+        ("(s)", ""),
+        ("(", "_"),
+        (")", ""),
+        ("/", "_"),
+        ("__", "_"),
+    ]
+
+    for k, v in replace:
+        txt = txt.replace(k, v)
+
+    txt = txt.lower().strip()
+
+    return txt
+
+
+def _preclean_seattle(df):
+    # cols
+    cols = [
+        "primarypropertytype",
+        "neighborhood",
+        "latitude",
+        "longitude",
+        "yearbuilt",
+        "numberofbuildings",
+        "numberoffloors",
+        "propertygfaparking",
+        "propertygfabuilding",
+        "energystarscore",
+        "siteenergyuse_kbtu",
+        "steamuse_kbtu",
+        "electricity_kbtu",
+        "naturalgas_kbtu",
+    ]
+
+    df = df.loc[:, cols]
+
+    # drop gfa and target negative
+    df = df.loc[df.propertygfabuilding.notna()]
+    df = df.loc[df.propertygfabuilding > 1, :]
+    df = df.loc[df.siteenergyuse_kbtu.notna()]
+    df = df.loc[df.siteenergyuse_kbtu > 1, :]
+
+    # fix floor building
+    df.numberofbuildings = df.numberofbuildings.fillna(1).astype(int).replace({0: 1})
+    df.numberoffloors = df.numberoffloors.fillna(1).astype(int).replace({0: 1})
+
+    # bool parking as nrj, stema elec etc
+    df.propertygfaparking = df.propertygfaparking.apply(lambda i: i > 0).astype(int)
+    df["bool_energystarscore"] = df.energystarscore.isna().astype(int)
+    df.steamuse_kbtu = df.steamuse_kbtu.apply(lambda i: i > 0).astype(int)
+    df.electricity_kbtu = df.electricity_kbtu.apply(lambda i: i > 0).astype(int)
+    df.naturalgas_kbtu = df.naturalgas_kbtu.apply(lambda i: i > 0).astype(int)
+
+    # feat eng
+    df["gfaperfloor"] = df.propertygfabuilding / df.numberoffloors
+    df["gfaperbuilding"] = df.propertygfabuilding / df.numberofbuildings
+
+    # df.primarypropertytype.value_counts()
+    df.primarypropertytype = df.primarypropertytype.replace(
+        {"office": "small- and mid-sized office"}
+    )
+
+    return df
+
+
+def _add_nan(df, nan_rate=0.0):
+    """ """
+
+    if nan_rate:
+        N = df.shape[0] * df.shape[1]
+        nan_numb = int(nan_rate * N)
+
+        for _ in range(nan_numb):
+            x, y = random.randint(0, df.shape[0] - 1), random.randint(
+                0, df.shape[1] - 1
+            )
+            df.iloc[x, y] = np.NaN
+
+    return df
